cc14d65d5ecf160a3596a33097b1ceece1220e03
Rashi Jaiswal <rashijaiswal6655@gmail.com>
2026-07-01 13:52:56 +0530

    complete ui

--- Frontend/src/modules/Food/pages/superadmin/layouts/Navbar.jsx
+++ Frontend/src/modules/Food/pages/superadmin/layouts/Navbar.jsx
@@ -24,7 +24,7 @@ export default function Navbar({ onToggleSidebar, isCollapsed }) {
   ]
 
   return (
-    <header className={`fixed top-0 left-0 w-full flex justify-between items-center px-4 md:px-6 h-13 bg-white dark:bg-zinc-900 border-b border-zinc-100 dark:border-zinc-805 z-40 shadow-sm transition-all duration-300 ${isCollapsed ? "lg:pl-[88px]" : "lg:pl-[296px]"}`}>
+    <header className={`fixed top-0 left-0 w-full flex justify-between lg:justify-end items-center px-4 md:px-6 h-13 bg-white dark:bg-zinc-900 border-b border-zinc-100 dark:border-zinc-805 z-40 shadow-sm transition-all duration-300 ${isCollapsed ? "lg:pl-[88px]" : "lg:pl-[296px]"}`}>
       
       {/* Left side: Brand + Toggle */}
       <div className="flex items-center gap-3 lg:hidden">
